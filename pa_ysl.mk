c047fe79876859e1fa42afbba90737e700ea7211
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-06-08 11:19:28 +0530

    Update and rename p404_ysl.mk to pa_ysl.mk

--- pa_ysl.mk
+++ pa_ysl.mk
@@ -16,19 +16,19 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from ysl device
 $(call inherit-product, device/xiaomi/ysl/device.mk)
 
-# Inherit some common P404 stuff.
+# Inherit some common PA stuff.
 
-$(call inherit-product, vendor/404/configs/common.mk)
+$(call inherit-product, vendor/pa/configs/common.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ysl
-PRODUCT_NAME := p404_ysl
+PRODUCT_NAME := pa_ysl
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi S2
 PRODUCT_MANUFACTURER := Xiaomi
